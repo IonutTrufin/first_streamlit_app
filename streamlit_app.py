1381a80eb68a8dfae9e100a150a59bdb320f2cef
IonutTrufin <Ionuttrufin@yahoo.com>
2023-11-16 18:16:02 +0200

    Update streamlit_app.py
    added emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit
 streamlit.title('New healthy dinner')
 streamlit.header('Breakfast menu')
-streamlit.text('Eggs and bacon')
-streamlit.text('Bread, butter and apricot jam')
+streamlit.text('🐔Eggs and bacon')
+streamlit.text('🍞 Bread, butter and apricot jam')
+streamlit.text('🥣 🥗 🐔 🥑 Some other stuff')
+
+
 
