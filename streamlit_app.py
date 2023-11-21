beca19e2103b3b6ed0ff1051c277571d33e1d3c6
IonutTrufin <Ionuttrufin@yahoo.com>
2023-11-21 19:16:16 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,18 +47,20 @@ except URLError as e:
 
 
 
-
-
-
-streamlit.stop()
-
-#import snowflake.connector
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_row = my_cur.fetchall()
 streamlit.header("The fruit list contains:")
-streamlit.dataframe(my_data_row)
+#function
+def get_fruit_load_list():
+     with my_cnx.cursor() as my_cur:
+          my_cur.execute("select * from fruit_load_list")
+          return my_cur.fetchall()
+
+#Button 
+if streamlit.button('Get fruit load list')
+     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+     my_data_row = get_fruit_load_list()
+     streamlit.dataframe(my_data_row)
+
+
 
 
 
